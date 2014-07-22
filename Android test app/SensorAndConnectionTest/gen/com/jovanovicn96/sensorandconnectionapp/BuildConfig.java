304193fb8e2d77cc83ecb7cc46bf4f97a8c3fcfa
Nikola Jovanović <asaknikola@gmail.com>
2014-07-22 14:50:15 +0200

    Full Android test app project added

---
+++ Android test app/SensorAndConnectionTest/gen/com/jovanovicn96/sensorandconnectionapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jovanovicn96.sensorandconnectionapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
