cb897e9d70e7742cc6cef04e01ec2fa30e050521
Stefan Nožinić <stefan@lugons.org>
2014-07-24 22:34:03 +0000

    Operate on integers instead of doubles

--- src/TestServer.cpp
+++ src/TestServer.cpp
@@ -10,17 +10,18 @@ int main(int argc, char * argv[])
 	UDPServer s(5000); 
 	while (true) 
 	{
-		char buf[49]; 
-		s.recieve(buf, 49); 
-		double ax,ay,az,ox,oy,oz; 
-		int n = sizeof(double);
+		char buf[25]; 
+		s.recieve(buf, 25); 
+		int ax,ay,az,ox,oy,oz; 
+		int n = sizeof(int);
 		memcpy(&ax, buf, n);
 		memcpy(&ay, buf+n, n);
 		memcpy(&az, buf+2*n, n);
 		memcpy(&ox, buf+3*n, n);
 		memcpy(&oy, buf+4*n, n);
 		memcpy(&oz, buf+5*n, n);
-		cout << ax << " " << ay << " " << az << " " << ox << " " << oy << " " << oz << buf[48] << endl; 
+		char indicator = buf[24];
+		cout << ax << " " << ay << " " << az << " " << ox << " " << oy << " " << oz << indicator << endl; 
 	}
 	return 0; 
 }
