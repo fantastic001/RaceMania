a468086a0f17a3ab8d4b1a6dee808b951c5c2b16
Stefan Nožinić <stefan@lugons.org>
2014-07-24 00:14:51 +0000

    UDP server class

---
+++ src/UDPServer.cpp
@@ -0,0 +1,38 @@
+
+#ifndef RACEMANIA_UDPSERVER_HPP 
+#define RACEMANIA_UDPSERVER_HPP
+
+
+#include "UDPServer.hpp" 
+
+using boost::asio::ip::udp;
+using namespace std; 
+UDPServer::UDPServer(int port = 5433) 
+{
+	m_socket = new udp::socket(m_io_service, udp::endpoint(udp::v4(), port));
+	m_port = port; 
+}
+UDPServer::~UDPServer() 
+{
+	delete m_socket; 
+	close();
+}
+
+void UDPServer::send(char* data, int size, string ip) 
+{
+	udp::endpoint destination(boost::asio::ip::address::from_string(ip), m_port);
+	m_socket->send_to(boost::asio::buffer(data, size), destination);
+}
+
+void UDPServer::recieve(char* data, int size) 
+{
+	udp::endpoint sender_endpoint;
+	m_socket->receive_from(boost::asio::buffer(data, size), sender_endpoint);
+}
+
+void UDPServer::close() 
+{
+	m_socket->close(); 
+}
+
+#endif // RACEMANIA_UDPSERVER_HPP
---
+++ src/UDPServer.hpp
@@ -0,0 +1,24 @@
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+#include <string>
+
+using boost::asio::ip::udp;
+
+class UDPServer 
+{
+	boost::asio::io_service m_io_service;
+	udp::socket *m_socket;
+	int m_port; 
+public: 
+	
+	UDPServer(); // Default constructor 
+	UDPServer(int); // Constructor with specified special port 
+	~UDPServer();; // destructor
+	
+	void send(char* , int, std::string); // send n bytes 
+
+	void recieve(char* , int); // recieve data
+
+	void close(); // close connection 
+};
